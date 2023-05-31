b9ba9fb6dc85ae33e0827f23915be747c3e19693
hojiyevcoder <133369186+hojiyevcoder@users.noreply.github.com>
2023-05-31 11:45:01 +0300

    Add files via upload

---
+++ email pochta tekshirish.py
@@ -0,0 +1,5 @@
+a=input("Iltimos email pochta manzilingizni kiriting = ")
+if a(len("@gmail.com")):
+    print("Sizning email pochtangiz yaroqli 👍👍👍")
+else:
+    print("Sizning email pochtangiz yaroqsiz")
